ce95fc696c587e01dd0747b45893feaff45e349d
Abraham Parker <abrahamemail@yahoo.com>
2023-12-31 13:03:35 -0500

    changedeventname

--- realworldvue/src/views/CreateEvent.vue
+++ realworldvue/src/views/CreateEvent.vue
@@ -4,19 +4,19 @@
       <h1>Create an Event</h1>
       <BaseSelect
         :options="categories"
-        v-model="event.category"
+        v-model="myEvent.category"
         label="Select a category"
       />
       <h3>Name & describe the event</h3>
       <!-- here BaseInput use just single use each time input for example title, then description-->
-      <BaseInput v-model="event.title" label="Title" type="text" />
-      <BaseInput v-model="event.description" label="Description" type="text" />
+      <BaseInput v-model="myEvent.title" label="Title" type="text" />
+      <BaseInput v-model="myEvent.description" label="Description" type="text" />
       <h3>Where is your event?</h3>
-      <BaseInput v-model="event.location" label="Location" type="text" />
+      <BaseInput v-model="myEvent.location" label="Location" type="text" />
       <h3>Are pets allowed?</h3>
       <div>
         <BaseRadio
-          v-model="event.pets"
+          v-model="myEvent.pets"
           :selectedValue="1"
           label="Yes"
           name="pets"
@@ -25,7 +25,7 @@
 
       <div>
         <BaseRadio
-          v-model="event.pets"
+          v-model="myEvent.pets"
           :selectedValue="0"
           label="No"
           name="pets"
@@ -33,8 +33,8 @@
       </div>
       <h3>Extras if any ...</h3>
       <div>
-        <BaseCheckbox v-model="event.extras.catering" label="Catering" />
-        <BaseCheckbox v-model="event.extras.music" label="Live Music" />
+        <BaseCheckbox v-model="myEvent.extras.catering" label="Catering" />
+        <BaseCheckbox v-model="myEvent.extras.music" label="Live Music" />
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -59,7 +59,7 @@ export default {
         "food",
         "community",
       ],
-      event: {
+      myEvent: {
         category: "",
         title: "",
         description: "",
