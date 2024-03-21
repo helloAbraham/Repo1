b889ed5a08df5d17afc627506f116d230a4eec59
Abraham Parker <abrahamemail@yahoo.com>
2024-03-21 14:20:43 -0400

    customeDate

--- realworldvue/src/router/index.js
+++ realworldvue/src/router/index.js
@@ -110,6 +110,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "FloatingLabelTextbox" */ "../components/FloatingLabelTextbox.vue"),
   },
+  {
+    path: "/cdate",
+    name: "CustomeSelectedDate",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "CustomeSelectedDate" */ "../views/CustomeSelectedDate.vue"),
+  },
   
 ];
 
